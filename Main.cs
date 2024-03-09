4552ce4542254e2725a1eb2b381a1363335bf730
Nether <enzonether@gmail.com>
2024-03-08 22:20:40 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Escreva sua altura em metros :");
+float altura = float.Parse(Console.ReadLine());
+Console.WriteLine("Escreva seu peso em Kg : ");
+int peso = int.Parse(Console.ReadLine());
+
+float IMC = peso / (altura * altura);
+
+Console.WriteLine($"Seu IMC é de : {IMC} KG/m²");
